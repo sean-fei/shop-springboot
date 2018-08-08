6d95de85cfe6f5f6662e22ea57d5448f7df43405
lirf <lirf@primeton.com>
2018-08-08 17:22:49 +0800

    init

--- shop-common/src/main/java/com/sean/shop/utils/BeanUtils.java
+++ shop-common/src/main/java/com/sean/shop/utils/BeanUtils.java
@@ -1,8 +1,73 @@
 package com.sean.shop.utils;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
 /**
- * @Author: Thinkpad
+ * @Author: sean
  * @CreateDate: 2018/8/8 14:24
  */
 public class BeanUtils {
+
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<String>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public static void copyPropertiesIgnoreNull(Object src, Object target) {
+        org.springframework.beans.BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
+    }
+
+    /**
+     * 复制集合
+     *
+     * @param <E>
+     * @param source
+     * @param destinationClass
+     * @return
+     * @throws InstantiationException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    public static <E> List<E> copyToPropertiesIgnoreNull(List<?> source, Class<E> destinationClass)
+            throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (source.size() == 0) {
+            return Collections.emptyList();
+        }
+        List<E> res = new ArrayList<E>(source.size());
+        for (Object o : source) {
+            E e = destinationClass.newInstance();
+            copyPropertiesIgnoreNull(o, e);
+            res.add(e);
+        }
+        return res;
+    }
+
+    public static <E> List<E> copyTo(List<?> source, Class<E> destinationClass)
+            throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (source.size() == 0) {
+            return Collections.emptyList();
+        }
+        List<E> res = new ArrayList<E>(source.size());
+        for (Object o : source) {
+            E e = destinationClass.newInstance();
+            org.springframework.beans.BeanUtils.copyProperties(o, e);
+            res.add(e);
+        }
+        return res;
+    }
+
 }
--- shop-common/src/main/java/com/sean/shop/utils/IDGenerator.java
+++ shop-common/src/main/java/com/sean/shop/utils/IDGenerator.java
@@ -1,8 +1,30 @@
 package com.sean.shop.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 /**
- * @Author: Thinkpad
+ * @Author: sean
  * @CreateDate: 2018/8/8 14:29
  */
 public class IDGenerator {
+
+    /**
+     * 生产UUID
+     * @return
+     */
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
+     * 生产业务CODE
+     * @return
+     */
+    public static String getBusinessCode() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        return sdf.format(new Date()).toString();
+    }
+
 }
